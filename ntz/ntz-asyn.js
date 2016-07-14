70cf5890cbcc764d40815a882e89c049dda956db
Netzulo.com <netzuleando@gmail.com>
2016-07-14 18:18:00 +0200

    @netzulo | FIX | v0.0.3.1
    Divide of listeners and emitter on server side

--- ntz/ntz-asyn.js
+++ ntz/ntz-asyn.js
@@ -37,39 +37,44 @@ module.exports = function Asyn(io){
 	}
 	//----
 	//CONFIGURACION PARA CADA SOCKET
-	definition.sockets.on('connection', function(socket){
-		console.log('[Socket.io]: Un usuario se ha conectado');
-
-		socket.emit('connected',{msg: "Usuario conectado"});
-
-		socket.on('contact', function (data) {
-			var response = { msg : "Conectado a la sala "+ chat.rooms[0].name};
-			//TODO: buscar una sala valida
-			chat.rooms[0].users.push({ id: "cli_001", name: data.name, cc: data.css });
-			chat.rooms[0].sockets.push(socket);
-			//Genero el canal de comunicacion privado
-			socket.join(chat.rooms[0].id);
-			//Se informa al cliente que esta conectado al nombre de sala
-			socket.broadcast.to(chat.rooms[0].id).emit('assigned', response);
-
-			//---
-			console.log("[SOCKET][contact]: %s", response.msg);			
-		});			
-		//----
-		socket.on('error', function(data) {
-			console.log("[Socket.io][ERROR]: error en evento recibido| socketID: %s | host: %s", this.client.conn.id,this.client.conn.remoteAddress);
-		});
-		//---
-	 	socket.on('disconnect', function(data){
-    		console.log('[Socket.io]: Un usuario se ha desconectado');
-  		});
-	});
+	definition.sockets.on('connection', onConnection);
 	//----	
 	//----
 	console.log("[Socket.io]: Cargado Socket.io");
 	//----	
 	return definition;
 };
+//-----------------------------------------------------------------
+function onConnection(socket){
+	console.log('[Socket.io]: Un usuario se ha conectado');	
+	//LISTENERs
+	socket.on('contact', onContact);			
+	socket.on('error', onError);
+	socket.on('disconnect', onDisconnect);
+	//EMITTERs
+	socket.emit('connected',{msg: "Usuario conectado"});
+}
+
+function onContact(data) {
+	var response = { msg : "Conectado a la sala "+ chat.rooms[0].name};
+	//TODO: buscar una sala valida
+	chat.rooms[0].users.push({ id: "cli_001", name: data.name, cc: data.css });
+	chat.rooms[0].sockets.push(socket);
+	//Genero el canal de comunicacion privado
+	socket.join(chat.rooms[0].id);
+	//Se informa al cliente que esta conectado al nombre de sala
+	socket.broadcast.to(chat.rooms[0].id).emit('assigned', response);
+	//---
+	console.log("[SOCKET][contact]: %s", response.msg);			
+}
+
+function onError(data) {
+	console.log("[Socket.io][ERROR]: error en evento recibido| socketID: %s | host: %s", this.client.conn.id,this.client.conn.remoteAddress);
+}
+
+function onDisconnect(data){
+	console.log('[Socket.io]: Un usuario se ha desconectado');
+}
 //-----------------------------------------------------------------
 function GetFreeAgent(byId){
 	var item = null;
@@ -87,4 +92,3 @@ function GetFreRoom(argument) {
 		}
 	}
 }
-
